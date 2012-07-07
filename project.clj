9b45f34ce842f79001b087984625198b66da3b96
missingfaktor <rahul.phulore.999@gmail.com>
2012-07-07 14:31:37 +0530

    JUNG stuff.

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject noba "0.1-SNAPSHOT"
   :description "Clojure port of the game Grand Theft Wumpus from Land of Lisp."
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [net.sf.jung/jung-api "2.0.1"]
+                 [net.sf.jung/jung-graph-impl "2.0.1"]
+                 [net.sf.jung/jung-algorithms "2.0.1"]
+                 [net.sf.jung/jung-visualization "2.0.1"]])
--- test/in/missingfaktor/noba/test.clj
+++ test/in/missingfaktor/noba/test.clj
@@ -1,9 +1,40 @@
 (ns in.missingfaktor.noba.test
   (:use [clojure.test]
-        [clojure.java.io]
-        [in.missingfaktor.noba graph dotifier game-data interface]))
+        [in.missingfaktor.noba graph dotifier game-data interface])
+  (:import [java.io File]
+           [java.awt Color Dimension]
+           [java.awt.image BufferedImage]
+           [javax.imageio ImageIO]
+           [edu.uci.ics.jung.graph SparseGraph]
+           [edu.uci.ics.jung.algorithms.layout CircleLayout]
+           [edu.uci.ics.jung.visualization VisualizationViewer]))
+
+(defn- save-graph-as-jpeg [graph file-name]
+  (let [lt (CircleLayout. graph)
+        vv (VisualizationViewer. lt)
+        size (.getPreferredSize vv)
+        width (.width size)
+        height (.height size)
+        img (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
+        gfx (.createGraphics img)]
+    (.fillRect gfx 0 0 width height)
+    (.paintAll vv gfx)
+    (.dispose gfx)
+    (ImageIO/write img "jpeg" (File. file-name))))
+
+(defn- probe-jung []
+  (let [g (SparseGraph.)]
+    (doto g
+      (.addVertex 1)
+      (.addVertex 2)
+      (.addVertex 5)
+      (.addEdge "Edge A" 1 2)
+      (.addEdge "Edge B" 2 5))
+    (println g)
+    (save-graph-as-jpeg g "testo.jpeg")))
 
 ; This test is but a placeholder.
 (deftest dotify-nodes-test
-  (dot>png (dotify-graph wizard-graph))
+  (probe-jung)
+  ;(dot>png (dotify-graph wizard-graph)) ; <-- Generates image but uses native shell.
   (is (dotify-identifier "hello$%df") "hello__df"))
