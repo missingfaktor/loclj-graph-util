61c3c8b40f7f64cc8a6f34c4c7df0ea62140c730
missingfaktor <rahul.phulore.999@gmail.com>
2012-07-09 02:01:21 +0530

    ..

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject loclj-graph-util "0.1-SNAPSHOT"
   :description "A simple Clojure port of graph-util from Chapter 7 of the book Land of Lisp."
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.sf.jung/jung-api "2.0.1"]
                  [net.sf.jung/jung-graph-impl "2.0.1"]
