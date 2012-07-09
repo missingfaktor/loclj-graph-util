664fc8194a7c823edac03c055f60657d4e907277
missingfaktor <rahul.phulore.999@gmail.com>
2012-07-09 19:17:19 +0530

    Added an option for AOT compilation.

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
                  [net.sf.jung/jung-api "2.0.1"]
                  [net.sf.jung/jung-graph-impl "2.0.1"]
                  [net.sf.jung/jung-algorithms "2.0.1"]
-                 [net.sf.jung/jung-visualization "2.0.1"]])
+                 [net.sf.jung/jung-visualization "2.0.1"]]
+  :aot :all)
